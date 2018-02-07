6b133205c8f50f07c947f05408f42c2a4066df8d
Felipe <felipejg8611@gmail.com>
2018-02-06 23:47:59 -0500

    Optimized imports.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,15 +1,12 @@
 package com.example.android.miwok;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
